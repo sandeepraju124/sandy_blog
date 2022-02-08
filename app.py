18b41dacece7462e6df4c60c733a0d25b89110d1
sandeepraju124 <68640943+sandeepraju124@users.noreply.github.com>
2022-02-08 21:20:01 +0530

    cleaned code

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask,render_template,url_for,request,redirect,flash,Response
 import json
+
 import pymongo
-from bson import json_util, ObjectId
-from flask import Flask,jsonify
+from flask import Flask
+from flask import render_template, request, Response
 
 client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
 db = client['sandeep']
 app = Flask(__name__)
 # db = client['sandeep']
-collection= db.employeeenformation
+collection = db.employeeenformation
 record = {
-    "sandeep":"smart",
-    "sai":"vijilint",
-    "fuck":"bohut hard"
+    "sandeep": "smart",
+    "sai": "vijilint",
+    "fuck": "bohut hard"
 }
+
+
 #
 # collection.insert_one(record)
 
@@ -24,48 +26,43 @@ record = {
 #     return render_template('index.html')
 
 
-
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         # content = request.get_json(force = True)
-        data = request.form.get("email")
         try:
-            collection.insert_one({"email":data})
+            data = request.form.get("email")
+            collection.insert_one({"email": data})
             return "inserted data"
         except Exception as e:
             print(e)
-            return (e)
+            return e
     return render_template('index.html')
 
 
-@app.route('/layer1',methods=['GET','POST'])
+@app.route('/layer1', methods=['GET', 'POST'])
 def layer1():
-    data = list(db.layer1.find())
-    response = []
-    for i in data:
-
-        i['_id'] = str(i['_id'])
-        response.append(i)
-    # print(response)
     try:
-        return Response(mimetype="application/json",
-        response=json.dumps({"data":data})
+        data = list(db.layer1.find())
+        response = []
+        for i in data:
+            i['_id'] = str(i['_id'])
+            response.append(i)
+        return Response(mimetype="application/json", status=500,
+                        response=json.dumps({"data": data})
 
-    )
+                        )
     except Exception as e:
         print(e)
-        return (e)
+        return e
 
 
-
-
-@app.route('/api/', subdomain ='api')
+@app.route('/api/', subdomain='api')
 def courses():
     return "under practice subdomain."
 
 
-@app.route('/test',methods=['GET', 'POST'])
+@app.route('/test', methods=['GET', 'POST'])
 def test():
     if request.method == 'POST':
         data = request.form.get('name')
@@ -73,12 +70,14 @@ def test():
         print('hit')
     return render_template('test.html')
 
-@app.route('/insertone',methods=['GET', 'POST'])
+
+@app.route('/insertone', methods=['GET', 'POST'])
 def insert():
     if request.method == 'POST':
-        data = request.form.get('email')
         return "inserted data"
 
     return 'data not get'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
